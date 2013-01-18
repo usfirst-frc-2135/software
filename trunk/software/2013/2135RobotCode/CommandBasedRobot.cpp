d0e5c329551bde5e200d34ecceb64aec987c6f35
jeff.l.mullins@gmail.com <jeff.l.mullins@gmail.com@1cb49900-12d1-9607-daca-1c963d2054bd>
2013-01-18 02:29:57 +0000

    Started dashboard testing. Changed joystick to use throttle routines. Made throttle print messages faster.

--- trunk/software/2013/2135RobotCode/CommandBasedRobot.cpp
+++ trunk/software/2013/2135RobotCode/CommandBasedRobot.cpp
@@ -1,19 +1,23 @@
 #include "WPILib.h"
 #include "Commands/Command.h"
+#include "Subsystems/Chassis.h"
 #include "CommandBase.h"
 //#include "Commands/Shoot.h"
 #include "Commands/DriveStraight.h"
+#include "DashboardDataFormat.h"
 
 class CommandBasedRobot : public IterativeRobot {
+	DashboardDataFormat dashboardDataFormat;
 
 private:
 	Command *autonomousCommand;
+	LiveWindow *lw;
 
 	virtual void RobotInit() {
-		CommandBase::init();
 		printf( "2135: RobotInit\n" );
+		CommandBase::init();
 		autonomousCommand = new DriveStraight(1);
-//		SmartDashboard::PutData( "SchedulerData", (Sendable*) Scheduler::GetInstance() );
+		lw = LiveWindow::GetInstance();
 	}
 	
 	virtual void AutonomousInit() {
@@ -24,9 +28,16 @@ private:
 	virtual void AutonomousPeriodic() {
 //		printf( "2135: AutonomousPeriodic\n" );
 		Scheduler::GetInstance()->Run();
+		dashboardDataFormat.SendIOPortData();
+		dashboardDataFormat.SendVisionData();
+		SmartDashboard::PutData( "SchedulerData", (Sendable*) Scheduler::GetInstance() );
 	}
 	
 	virtual void TeleopInit() {
+		// This makes sure that the autonomous stops running when
+		// teleop starts running. If you want the autonomous to 
+		// continue until interrupted by another command, remove
+		// this line or comment it out.
 		printf( "2135: TeleopInit\n" );
 		autonomousCommand->Cancel();
 	}
@@ -34,11 +45,15 @@ private:
 	virtual void TeleopPeriodic() {
 //		printf( "2135: TeleopPeriodic\n" );
 		Scheduler::GetInstance()->Run();
+		dashboardDataFormat.SendIOPortData();
+		dashboardDataFormat.SendVisionData();
+		SmartDashboard::PutData( "SchedulerData", (Sendable*) Scheduler::GetInstance() );
+//		SmartDashboard::PutData( chassis );
 	}
 	
 	virtual void TestPeriodic() {
 //		printf( "2135: TestPeriodic\n" );
-		Scheduler::GetInstance()->Run();
+		lw->Run();
 	}
 };
 
