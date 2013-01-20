0d8747e239c2f14366d939fd60fa908a7c442eb6
jeff.l.mullins@gmail.com <jeff.l.mullins@gmail.com@1cb49900-12d1-9607-daca-1c963d2054bd>
2013-01-20 23:54:46 +0000

    Fixed shooter power settings. Added ShooterOff command.

--- trunk/software/2013/2135RobotCode/Subsystems/Shooter.cpp
+++ trunk/software/2013/2135RobotCode/Subsystems/Shooter.cpp
@@ -7,6 +7,7 @@
 #include "../Robotmap.h"
 #include "Shooter.h"
 #include "../Commands/Shoot.h"
+#include "../Commands/ShooterOff.h"
 
 Shooter::Shooter() : Subsystem("Shooter") {
 	printf( "2135: Shooter - constructed\n" );
@@ -18,24 +19,25 @@ Shooter::Shooter() : Subsystem("Shooter") {
     
 void Shooter::InitDefaultCommand() {
 	// Set the default command for a subsystem here.
-	SetDefaultCommand ( new Shoot( 0 ) );
+	SetDefaultCommand ( new ShooterOff() );
 }
 
 // Put methods for controlling this subsystem
 // here. Call these from Commands.
 
-void Shooter::SetPower( float intakePower, float exitPower ) {
+void Shooter::SetPower( float intakeJoystick, float exitJoystick ) {
 	static	int i = 0;
-	float	intakePowerPercent;
-	float	exitPowerPercent;
+	float	intakePower;
+	float	exitPower;
 
+	intakePower = - ( intakeJoystick - 1) / 2;
+	exitPower = - ( exitJoystick - 1 ) / 2;
 	shooterIntakeMotor->Set( intakePower, 0 );
 	shooterExitMotor->Set( exitPower, 0 );
 	
-	intakePowerPercent = - ( intakePower - 1) / 2 * 100;
-	exitPowerPercent = - ( exitPower - 1 ) / 2 * 100;
-	
-	if ( i++ > 20 ) {i = 0 ;
-		printf( "Set shooter power: %6.1f  %6.1f\n", intakePowerPercent, exitPowerPercent );
+	if ( i++ > 20 ) {
+		i = 0 ;
+		printf( "2135: Shooter: Set shooter power: %6.1f  %6.1f\n",
+			intakePower * 100, exitPower * 100 );
 	}
 }
