4f522cd5f7282d951727b5d584bc9ca575a8d4da
jeff.l.mullins@gmail.com <jeff.l.mullins@gmail.com@1cb49900-12d1-9607-daca-1c963d2054bd>
2013-02-01 00:31:27 +0000

    DriveWithJoystick working

--- 2013/RobotCode/Commands/DriveWithJoystick.cpp
+++ 2013/RobotCode/Commands/DriveWithJoystick.cpp
@@ -21,7 +21,7 @@ void DriveWithJoystick::Initialize() {
 }
 // Called repeatedly when this Command is scheduled to run
 void DriveWithJoystick::Execute() {
-	
+	Robot::chassis->driveWithJoystick(Robot::oi->getDriverStick());
 }
 // Make this return true when this Command no longer needs to run execute()
 bool DriveWithJoystick::IsFinished() {
