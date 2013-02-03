f2936eadc830918ccf319ed3670bf2b25e5b9217
jeff.l.mullins@gmail.com <jeff.l.mullins@gmail.com@1cb49900-12d1-9607-daca-1c963d2054bd>
2013-02-03 05:56:28 +0000

    RobotBuilder configuration file synchronization. Changed operator trigger from ShootFrisbee to ShooterSetSpeed. Enabled motor safety now that DriveWithJoystick command has been implemented. Various spacing changes due to RobotBuilder export changes.

--- 2013/RobotCode/Commands/ShootFrisbee.cpp
+++ 2013/RobotCode/Commands/ShootFrisbee.cpp
@@ -22,7 +22,6 @@ void ShootFrisbee::Initialize() {
 // Called repeatedly when this Command is scheduled to run
 void ShootFrisbee::Execute() {
 }			
-
 // Make this return true when this Command no longer needs to run execute()
 bool ShootFrisbee::IsFinished() {
 	return false;
