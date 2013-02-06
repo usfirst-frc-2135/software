4dde36ce9b633bc2f6be148e13a33ffee0e180e9
jeff.l.mullins@gmail.com <jeff.l.mullins@gmail.com@1cb49900-12d1-9607-daca-1c963d2054bd>
2013-02-06 02:02:17 +0000

    Made a quick patch of the MagazineLoadSlot1 IsFinished function.

--- trunk/software/2013/RobotCode/Commands/MagazineStop.cpp
+++ trunk/software/2013/RobotCode/Commands/MagazineStop.cpp
@@ -26,7 +26,7 @@ void MagazineStop::Execute() {
 }
 // Make this return true when this Command no longer needs to run execute()
 bool MagazineStop::IsFinished() {
-	return true;
+	return false;
 }
 // Called once after isFinished returns true
 void MagazineStop::End() {
