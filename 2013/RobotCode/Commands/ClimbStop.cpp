acc55b7693daa4df8b7d70225e97b9443cf2a9de
jeff.l.mullins@gmail.com <jeff.l.mullins@gmail.com@1cb49900-12d1-9607-daca-1c963d2054bd>
2013-02-06 06:03:46 +0000

    Added motor stop calls for all stop commands. Created a new MyOnTarget command in Magazine.cpp to replace the PID controller call in WPILib that has a critical section bug.

--- 2013/RobotCode/Commands/ClimbStop.cpp
+++ 2013/RobotCode/Commands/ClimbStop.cpp
@@ -21,7 +21,7 @@ void ClimbStop::Initialize() {
 }
 // Called repeatedly when this Command is scheduled to run
 void ClimbStop::Execute() {
-	
+	RobotMap::climberClimberMotor->Set( 0.0, 0 );
 }
 // Make this return true when this Command no longer needs to run execute()
 bool ClimbStop::IsFinished() {
--- 2013/RobotCode/Commands/TargetStop.cpp
+++ 2013/RobotCode/Commands/TargetStop.cpp
@@ -21,7 +21,7 @@ void TargetStop::Initialize() {
 }
 // Called repeatedly when this Command is scheduled to run
 void TargetStop::Execute() {
-	
+	RobotMap::targetingElevationMotor->Set( 0.0, 0 );
 }
 // Make this return true when this Command no longer needs to run execute()
 bool TargetStop::IsFinished() {
