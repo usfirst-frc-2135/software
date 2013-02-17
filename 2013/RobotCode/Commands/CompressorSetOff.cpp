ad98edcf7214a9148bce74739636025ff4cdcafb
jeff.l.mullins@gmail.com <jeff.l.mullins@gmail.com@1cb49900-12d1-9607-daca-1c963d2054bd>
2013-02-17 05:45:26 +0000

    Removed magazine subsystem. Removed sweeper subsystem. Removed camera servo. Removed entry shooter encoder.

--- 2013/RobotCode/Commands/CompressorSetOff.cpp
+++ 2013/RobotCode/Commands/CompressorSetOff.cpp
@@ -18,6 +18,7 @@ CompressorSetOff::CompressorSetOff() {
 // Called just before this Command runs the first time
 void CompressorSetOff::Initialize() {
 	printf( "2135: CompressorSetOff\n");
+	Robot::pneumatics->stopCompressor();
 }
 // Called repeatedly when this Command is scheduled to run
 void CompressorSetOff::Execute() {
