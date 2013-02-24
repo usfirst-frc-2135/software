ba930e1f0244eb8902994a69a4f6ba5d84fd93ef
jeff.l.mullins@gmail.com <jeff.l.mullins@gmail.com@1cb49900-12d1-9607-daca-1c963d2054bd>
2013-02-24 06:25:41 +0000

    Increased fixed frisbee shooter power. Added more button controls for spin turns. Fixed firing pin command button.

--- 2013/RobotCode/Commands/ShootFrisbee.cpp
+++ 2013/RobotCode/Commands/ShootFrisbee.cpp
@@ -22,7 +22,7 @@ void ShootFrisbee::Initialize() {
 }
 // Called repeatedly when this Command is scheduled to run
 void ShootFrisbee::Execute() {
-	Robot::shooter->SetPower( 0.4, 0.7 );
+	Robot::shooter->SetPower( 0.5, 0.75 );
 }			
 // Make this return true when this Command no longer needs to run execute()
 bool ShootFrisbee::IsFinished() {
