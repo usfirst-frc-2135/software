77d78ec68ec3e766bf2b3287d4c2c7a0517d9f73
jeff.l.mullins@gmail.com <jeff.l.mullins@gmail.com@1cb49900-12d1-9607-daca-1c963d2054bd>
2014-03-04 07:09:06 +0000

    Updated commands to have print statements.

--- trunk/software/2014/Robot2014-Atlas/RobotMap.h
+++ trunk/software/2014/Robot2014-Atlas/RobotMap.h
@@ -28,11 +28,11 @@ public:
 	static DoubleSolenoid* transmissionShifterSolenoid;
 	static DoubleSolenoid* sweeperExtendSolenoid;
 	static SpeedController* sweeperWheelMotor;
-	static DoubleSolenoid* shooterReleaseSolenoid;
 	static SpeedController* shooterRetractMotor;
 	static DigitalInput* shooterRetractLimit1;
 	static DigitalInput* shooterRetractLimit2;
 	static Encoder* shooterRetractEncoder;
+	static DoubleSolenoid* shooterReleaseSolenoid;
 	static DoubleSolenoid* ballGateCloseSolenoid;
 	static DigitalInput* ballGateBallDetectLimit;
 	static Compressor* pneumaticsCompressor;
