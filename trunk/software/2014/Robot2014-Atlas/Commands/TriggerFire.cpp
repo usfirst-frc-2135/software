0fa2ceec21a55b69687b6fbe665ec8de93378a75
jeff.l.mullins@gmail.com <jeff.l.mullins@gmail.com@1cb49900-12d1-9607-daca-1c963d2054bd>
2014-03-05 08:35:39 +0000

    Updated shooter trigger methods and add print statements.

--- trunk/software/2014/Robot2014-Atlas/Commands/TriggerFire.cpp
+++ trunk/software/2014/Robot2014-Atlas/Commands/TriggerFire.cpp
@@ -17,11 +17,11 @@ TriggerFire::TriggerFire() {
 }
 // Called just before this Command runs the first time
 void TriggerFire::Initialize() {
-	
+	printf( "2135: Trigger Fire\n" );
 }
 // Called repeatedly when this Command is scheduled to run
 void TriggerFire::Execute() {
-	
+	Robot::triggerShooter->Fire();
 }
 // Make this return true when this Command no longer needs to run execute()
 bool TriggerFire::IsFinished() {
