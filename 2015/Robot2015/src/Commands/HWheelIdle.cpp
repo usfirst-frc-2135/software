c07245281ccbf9a55e086bc245076248e2e975c4
jeff.l.mullins@gmail.com <jeff.l.mullins@gmail.com@1cb49900-12d1-9607-daca-1c963d2054bd>
2015-03-08 00:50:42 +0000

    added printf to every initialize in commands

--- 2015/Robot2015/src/Commands/HWheelIdle.cpp
+++ 2015/Robot2015/src/Commands/HWheelIdle.cpp
@@ -22,7 +22,8 @@ HWheelIdle::HWheelIdle() {
 
 // Called just before this Command runs the first time
 void HWheelIdle::Initialize() {
-	
+	printf("2135: HWheel Idle\n");
+
 }
 
 // Called repeatedly when this Command is scheduled to run
