d3cf0aa64e509f850637cf296d8639123e371b21
jeff.l.mullins@gmail.com <jeff.l.mullins@gmail.com@1cb49900-12d1-9607-daca-1c963d2054bd>
2015-04-04 04:54:54 +0000

    Added auto recycling bin command.

--- 2015/Robot2015/src/Commands/AutoRecyclingBin.cpp
+++ 2015/Robot2015/src/Commands/AutoRecyclingBin.cpp
@@ -11,6 +11,7 @@
 
 
 #include "AutoRecyclingBin.h"
+#include "../Commands/DriveDistanceTimed.h"
 
 AutoRecyclingBin::AutoRecyclingBin() {
 	// Add Commands here:
@@ -29,4 +30,6 @@ AutoRecyclingBin::AutoRecyclingBin() {
 	// e.g. if Command1 requires chassis, and Command2 requires arm,
 	// a CommandGroup containing them would require both the chassis and the
 	// arm.
+	AddSequential(new DriveDistanceTimed(1.25, DriveDistanceTimed::kDriveForward));
+	AddSequential(new DriveDistanceTimed(1.5, DriveDistanceTimed::kDriveReverse));
 }
