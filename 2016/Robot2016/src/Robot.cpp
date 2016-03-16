596bba33b094304396394c7c6cda3d270232feea
Jeff Mullins <jeff.mullins@comcast.net>
2016-03-15 23:55:57 -0700

    Fixed camera conflict

--- 2016/Robot2016/src/Robot.cpp
+++ 2016/Robot2016/src/Robot.cpp
@@ -76,13 +76,9 @@ void Robot::RobotInit() {
 
 	printf("2135: Building autonomous chooser complete\n");
 
-<<<<<<< HEAD
 	// Start up the camera
 	CameraServer::GetInstance()->StartAutomaticCapture("cam1");
-=======
-//	CameraServer::GetInstance()->StartAutomaticCapture("cam0");
->>>>>>> branch 'master' of https://github.com/usfirst-frc-2135/software.git
-  }
+}
 
 /**
  * This function is called when the disabled button is hit.
