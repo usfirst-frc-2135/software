ec0b23ae0485968db514302ce31852d6b5c2712e
Presentation Robotics <frc2135@github.com>
2018-01-13 15:03:36 -0800

    Added buttons 4 & 5 on joystick for left and right drive spin.

---
+++ 2018/Robot2018/src/RobotDefaults.h
@@ -0,0 +1,17 @@
+/*
+ * RobotDefaults.h
+ *
+ *  Created on: Jan 13, 2018
+ *      Author: Administrator
+ */
+
+#ifndef ROBOTDEFAULTS_H_
+#define ROBOTDEFAULTS_H_
+
+// SmartDashboard Chassis Basics
+
+#define CHS_DRIVE_SPIN		"Chs_DriveSpin"
+#define CHS_DRIVE_SPIN_D	0.5
+
+
+#endif /* ROBOTDEFAULTS_H_ */
