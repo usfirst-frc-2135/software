3f1b64696bffdfa69c129e570fa7cadf7c4734d5
Presentation Robotics <robotics@presentationhs.org>
2018-03-24 23:42:06 -0700

    Corrected build chooser command to finish immediately

--- 2018/Robot2018/src/Commands/BuildChooser.cpp
+++ 2018/Robot2018/src/Commands/BuildChooser.cpp
@@ -34,7 +34,7 @@ void BuildChooser::Execute() {
 
 // Make this return true when this Command no longer needs to run execute()
 bool BuildChooser::IsFinished() {
-    return false;
+    return true;
 }
 
 // Called once after isFinished returns true
