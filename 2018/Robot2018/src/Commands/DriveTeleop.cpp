a16f1d5b04faea14b5bbfb77e8222d4bc018f1ba
Presentation Robotics <robotics@presentationhs.org>
2018-11-05 21:25:46 -0800

    Split driver joystick between turning and throttle

--- 2018/Robot2018/src/Commands/DriveTeleop.cpp
+++ 2018/Robot2018/src/Commands/DriveTeleop.cpp
@@ -29,7 +29,7 @@ void DriveTeleop::Initialize() {
 
 // Called repeatedly when this Command is scheduled to run
 void DriveTeleop::Execute() {
-	Robot::drivetrain->MoveWithJoystick(Robot::oi->getDStick());
+	Robot::drivetrain->MoveWithJoystick(Robot::oi->getDStick(), Robot::oi->getDStick2());
 }
 
 // Make this return true when this Command no longer needs to run execute()
