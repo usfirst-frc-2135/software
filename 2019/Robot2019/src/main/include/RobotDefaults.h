b94ad530949c8f1594410e4018bfb3ca1f9bd2d1
Presentation Invasion <presentation.robotics@gmail.com>
2019-02-05 18:18:30 -0800

    Created Robot Defaults; made basic autonomous command groups (blank templates) for each position and level to the cargo ship.

---
+++ 2019/Robot2019/src/main/include/RobotDefaults.h
@@ -0,0 +1,21 @@
+/*
+ * RobotDefaults.h
+ *
+ *  Created on: February 5, 2019
+ *      Author: PHS_User
+ *      Purpose: storage of constants 
+ */
+
+#ifndef ROBOTDEFAULTS_H_
+#define ROBOTDEFAULTS_H_
+
+#ifndef M_PI
+    #define M_PI 3.14159265358979323846
+#endif
+
+//  SmartDashboard string definitions
+
+#define AUTO_DT_DD          "DT_DDist"
+#define AUTO_DT_DT          "DT_DTurn"
+
+
