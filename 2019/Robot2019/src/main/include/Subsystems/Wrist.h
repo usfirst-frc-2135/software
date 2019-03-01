d2daddbe959a973b0e57e66bb4c68fbaf2ccb78e
Jeff Mullins <jeff.l.mullins@gmail.com>
2019-02-28 23:48:44 -0800

    Changed sprocket reduction to 1:1 as designed

--- 2019/Robot2019/src/main/include/Subsystems/Wrist.h
+++ 2019/Robot2019/src/main/include/Subsystems/Wrist.h
@@ -38,7 +38,7 @@ private:
 	const int 		m_slotIndex = 0;				// Motor controller profile slot index
 	const int 		m_pidIndex = 0; 				// PID Slot index for sensors
 	const int 		m_timeout = 30;					// CAN timeout in msec to wait for response
-	const double	OUTPUT_SHAFT_REDUCTION = ((double)22/(double)16);	// Sprocket reduction on output shaft 16T to 22T sprockets
+	const double	OUTPUT_SHAFT_REDUCTION = ((double)16/(double)16);	// Sprocket reduction on output shaft 16T to 16T sprockets
 	const double 	COUNTS_PER_ROTATION = (1024 * 4); 	// CPR is 1024 and multipled by 4 because it is a quadrature encoder
    
     bool			m_talonValidWR12;				// Health indicator for wrist Talon 12
