03dc4155ecb60620bc6d9245ff45f1cbf8989e20
Presentation Robotics <robotics@presentationhs.org>
2019-03-08 11:30:22 -0800

    Changed Auto Stop to finish immediately, to allow driving during sandstorm period.

--- 2019/Robot2019/src/main/cpp/Commands/AutoStop.cpp
+++ 2019/Robot2019/src/main/cpp/Commands/AutoStop.cpp
@@ -34,7 +34,7 @@ void AutoStop::Execute() {
 
 // Make this return true when this Command no longer needs to run execute()
 bool AutoStop::IsFinished() {
-    return false;
+    return true;
 }
 
 // Called once after isFinished returns true
