22053182128d76f9466b205ac1a67d526bc51e1d
Presentation Robotics <robotics@presentationhs.org>
2019-04-19 17:16:48 -0700

    Split VisionLoop into thread and stream handling in GripVisionLoop and frame processing in GripOuterPipeline for compatibility with RPi This is still commented out in the main Robot class and should not run unless uncommented Still needs to be tested on RoboRIO - this is only the initial check-in that compiles cleanly

--- 2019/Robot2019/src/main/include/frc2135/GripContoursPipeline.h
+++ 2019/Robot2019/src/main/include/frc2135/GripContoursPipeline.h
@@ -1,10 +1,10 @@
 #pragma once
-#include "vision/VisionPipeline.h"
+
+#include "RobotDefaults.h"
 
 #include <opencv2/objdetect/objdetect.hpp>
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
-//#include <opencv2/contrib/contrib.hpp>
 #include <opencv2/core/core.hpp>
 #include <opencv2/features2d.hpp>
 #include <iostream>
@@ -14,7 +14,6 @@
 #include <vector>
 #include <string>
 #include <math.h>
-#include <frc/WPILib.h>
 
 namespace grip {
 
@@ -27,9 +26,11 @@ class GripContoursPipeline : public frc::VisionPipeline {
 	private:
 		cv::Mat hslThresholdOutput;
 		std::vector<std::vector<cv::Point> > findContoursOutput;
+		std::vector<std::vector<cv::Point> > convexHullsOutput;
 		std::vector<std::vector<cv::Point> > filterContoursOutput;
 		void hslThreshold(cv::Mat &, double [], double [], double [], cv::Mat &);
 		void findContours(cv::Mat &, bool , std::vector<std::vector<cv::Point> > &);
+		void convexHulls(std::vector<std::vector<cv::Point> > &, std::vector<std::vector<cv::Point> > &);
 		void filterContours(std::vector<std::vector<cv::Point> > &, double , double , double , double , double , double , double [], double , double , double , double , std::vector<std::vector<cv::Point> > &);
 
 		cv::Mat source0;
@@ -37,10 +38,11 @@ class GripContoursPipeline : public frc::VisionPipeline {
 
 	public:
 		GripContoursPipeline();
-		virtual void Process(cv::Mat& mat);
-		cv::Mat* gethslThresholdOutput();
-		std::vector<std::vector<cv::Point> >* getfindContoursOutput();
-		std::vector<std::vector<cv::Point> >* getfilterContoursOutput();
+		void Process(cv::Mat& source0) override;
+		cv::Mat* GetHslThresholdOutput();
+		std::vector<std::vector<cv::Point> >* GetFindContoursOutput();
+		std::vector<std::vector<cv::Point> >* GetConvexHullsOutput();
+		std::vector<std::vector<cv::Point> >* GetFilterContoursOutput();
 };
 
 
