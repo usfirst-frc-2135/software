6a9d7f0eb0d9d4734e65749d3c96ad2a33368015
Presentation Robotics <frc2135@github.com>
2020-01-18 14:15:57 -0800

    This is the trajectory code. The pathway has not been added to this yet.

---
+++ 2020/TrajectoryNew(Independent)/src/main/include/RobotDefaults.h
@@ -0,0 +1,35 @@
+/*
+ * RobotDefaults.h
+ *
+ *  Created on: February 5, 2019
+ *      Author: PHS_User
+ *      Purpose: storage of constants 
+ */
+
+#pragma once
+
+#ifndef M_PI
+#define M_PI 3.14159265358979323846
+#endif
+
+//    SmartDashboard Vision settings
+
+#define CAM_HUESTART_D  50.0
+#define CAM_HUEEND_D    102.0
+
+#define CAM_SATSTART_D  110.0
+#define CAM_SATEND_D    255.0
+
+#define CAM_LUMSTART_D  44.0
+#define CAM_LUMEND_D    255.0
+
+//    SmartDashboard Vision readout
+
+#define CAM_DISTANCE        "Cam_Distance"
+#define CAM_DISTANCE_D      0.0
+
+#define	CAM_YAWANGLE       "Cam_YawAngle"
+#define	CAM_YAWANGLE_D     0.0
+
+#define CAM_FOUNDGOAL   "Cam_FoundGoal"
+#define CAM_TICKS       "Cam_Ticks"
