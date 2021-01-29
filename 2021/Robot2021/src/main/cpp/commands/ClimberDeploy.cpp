597bd8bf50c82a40227d5d51ad477cf104ac918e
Savannah Fujimoto <sfujimoto.22@presentationhs.org>
2021-01-28 16:13:52 -0800

    Fixed ClimberDeploy command to return true for IsFinished method

--- 2021/Robot2021/src/main/cpp/commands/ClimberDeploy.cpp
+++ 2021/Robot2021/src/main/cpp/commands/ClimberDeploy.cpp
@@ -42,7 +42,7 @@ void ClimberDeploy::Execute()
 // Make this return true when this Command no longer needs to run execute()
 bool ClimberDeploy::IsFinished()
 {
-    return false;
+    return true;
 }
 
 // Called once after isFinished returns true
