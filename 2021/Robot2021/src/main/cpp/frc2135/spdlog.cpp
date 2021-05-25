4d13f314d0c4e3ec03255bc47114dbc712f6b84b
Connor Worley <connorbworley@gmail.com>
2021-05-25 10:04:13 -0700

    Fix spdlog logs/ directory on roboRIO

--- 2021/Robot2021/src/main/cpp/frc2135/spdlog.cpp
+++ 2021/Robot2021/src/main/cpp/frc2135/spdlog.cpp
@@ -1,10 +1,12 @@
 #include <chrono>
+#include <frc/Filesystem.h>
 #include <spdlog/async.h>
 #include <spdlog/fmt/chrono.h>
 #include <spdlog/sinks/basic_file_sink.h>
 #include <spdlog/sinks/stdout_color_sinks.h>
 #include <spdlog/spdlog.h>
 #include <string>
+#include <wpi/SmallString.h>
 
 namespace frc2135
 {
@@ -16,25 +18,38 @@ namespace frc2135
 
     void initialize_spdlog()
     {
-        spdlog::init_thread_pool(256, 1);
-        spdlog::flush_every(std::chrono::seconds(1));
+        wpi::SmallString<64> operating_directory;
+        frc::filesystem::GetOperatingDirectory(operating_directory);
 
-        auto basic_file_logger = std::make_shared<spdlog::sinks::basic_file_sink_mt>(
-            fmt::format("logs/{:%Y-%m-%d_%H-%M-%S}.log", std::chrono::system_clock::now()));
-        auto stdout_logger = std::make_shared<spdlog::sinks::stdout_color_sink_mt>();
+        try
+        {
+            spdlog::init_thread_pool(256, 1);
+            spdlog::flush_every(std::chrono::seconds(1));
 
-        auto logger = std::make_shared<spdlog::async_logger>(
-            "2135",
-            spdlog::sinks_init_list{
-                basic_file_logger,
-                stdout_logger,
-            },
-            spdlog::thread_pool(),
-            spdlog::async_overflow_policy::overrun_oldest);
+            auto basic_file_logger = std::make_shared<spdlog::sinks::basic_file_sink_mt>(fmt::format(
+                "{}/logs/{:%Y-%m-%d_%H-%M-%S}.log",
+                operating_directory.c_str(),
+                std::chrono::system_clock::now()));
+            auto stdout_logger = std::make_shared<spdlog::sinks::stdout_color_sink_mt>();
 
-        spdlog::set_default_logger(logger); // keeps the logger alive
-        spdlog::set_error_handler(handle_spdlog_error);
+            auto logger = std::make_shared<spdlog::async_logger>(
+                "2135",
+                spdlog::sinks_init_list{
+                    basic_file_logger,
+                    stdout_logger,
+                },
+                spdlog::thread_pool(),
+                spdlog::async_overflow_policy::overrun_oldest);
 
-        spdlog::info("spdlog initialized");
+            spdlog::set_default_logger(logger); // keeps the logger alive
+            spdlog::set_error_handler(handle_spdlog_error);
+
+            spdlog::info("spdlog initialized");
+        }
+        catch (spdlog::spdlog_ex e)
+        {
+            // If initialization fails,  print the exception and fall back to the default stdout logger
+            spdlog::error("*** SPDLOG EXCEPTION DURING INITIALIZATION: {} ***", e.what());
+        }
     }
 } // namespace frc2135
