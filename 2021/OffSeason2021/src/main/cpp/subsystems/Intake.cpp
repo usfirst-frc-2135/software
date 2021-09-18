2cd126d2579513ee82b0f34700a534c84418d52e
Presentation Robotics <robotics@presentationhs.org>
2021-09-17 19:31:48 -0700

    Changed intake, floor conveyor, and vertical conveyor to use full speed in acquire direction and -0.2 in expel direction per Grogu slides

--- 2021/OffSeason2021/src/main/cpp/subsystems/Intake.cpp
+++ 2021/OffSeason2021/src/main/cpp/subsystems/Intake.cpp
@@ -43,8 +43,8 @@ Intake::Intake()
 
     // Initialize Variables
     frc2135::RobotConfig *config = frc2135::RobotConfig::GetInstance();
-    config->GetValueAsDouble("IN_AcquireSpeed", m_acquireSpeed, 0.8);
-    config->GetValueAsDouble("IN_ExpelSpeed", m_expelSpeed, -0.8);
+    config->GetValueAsDouble("IN_AcquireSpeed", m_acquireSpeed, 1.0);
+    config->GetValueAsDouble("IN_ExpelSpeed", m_expelSpeed, -0.2);
 
     // Set motor directions
     // Turn on Coast mode (not brake)
