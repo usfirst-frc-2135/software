9135aabd5152a8f1b59a9b8ba04cc16a1a10cd45
Alisha Shanawaz <ashanawaz.22@presentationhs.org>
2021-09-20 15:50:12 -0700

    Fixed code on Shooter subsystem.

--- 2021/OffSeason2021/src/main/cpp/commands/ShooterRun.cpp
+++ 2021/OffSeason2021/src/main/cpp/commands/ShooterRun.cpp
@@ -39,7 +39,7 @@ void ShooterRun::Execute() {}
 // Make this return true when this Command no longer needs to run execute()
 bool ShooterRun::IsFinished()
 {
-    return false;
+    return true;
 }
 
 // Called once after isFinished returns true
